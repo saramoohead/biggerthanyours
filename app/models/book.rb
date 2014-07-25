7a55d410c10bdc3604604be620c26c6bc65119d2
Tim McDonald-Bell <tim@steer.me>
2014-07-25 20:45:18 +0100

    Add paperclip to model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,7 @@ class Book < ActiveRecord::Base
 	validates :isbn, presence: true
 	validates :title, presence: true
 
+  has_attached_file :cover, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 end
