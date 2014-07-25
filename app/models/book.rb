0464c6c3dd9cdeab2bcbe64e95e3208be5111851
Tim McDonald-Bell <tim@steer.me>
2014-07-25 21:06:23 +0100

    simple form

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,6 @@ class Book < ActiveRecord::Base
 	validates :title, presence: true
 
   has_attached_file :cover, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
 
 end
